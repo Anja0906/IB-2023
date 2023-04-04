04ee3e3b9b23986918c736a8edf73927126ea2d8
Anja Petković <anjapetkovic92@gmail.com>
2023-04-04 10:01:15 +0200

    Fixed bug

--- ib/ib/src/main/java/com/ib/ib/model/User.java
+++ ib/ib/src/main/java/com/ib/ib/model/User.java
@@ -9,9 +9,6 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
-@Setter
-@Getter
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,6 +32,81 @@ public class User {
     public User() {
     }
 
+    public User(Integer id, String email, String firstName, String lastName, String telephoneNumber, String passwordHash, boolean activated, boolean admin) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephoneNumber = telephoneNumber;
+        this.passwordHash = passwordHash;
+        this.activated = activated;
+        this.admin = admin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -43,6 +115,9 @@ public class User {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", telephoneNumber='" + telephoneNumber + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", activated=" + activated +
+                ", admin=" + admin +
                 '}';
     }
 }
