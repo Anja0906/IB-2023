fbf78003991c27050929b25044ccacf0c6b2b2c4
AlexanderMishutkin <alex.type59@gmail.com>
2023-05-08 11:49:03 +0200

    Self-XSS warning

--- ib-front/ib-front/src/app/app.component.ts
+++ ib-front/ib-front/src/app/app.component.ts
@@ -9,4 +9,10 @@ import {CertificateService} from "./certificate-cervice/certificate.service";
 })
 export class AppComponent {
   title = 'ib-front';
+
+  ngOnInit() {
+    // Self-XSS warning
+    console.log("%cSTOP!", "font-size: 50px; font-weight: bold; color: red;");
+    console.log("%cThis is a browser feature intended for developers. If someone told you to copy-paste something here to enable a feature or hack someone's account, it is a scam and will give them access to your account.", "font-size: 16px; color: red;");
+  }
 }
